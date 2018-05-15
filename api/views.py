70a0b76d330e00e51da6fad542d4992fcd6afd90
nsotl <chenweiji57740@gmail.com>
2018-05-15 08:59:20 +0800

    fix get_base_features, add session

--- api/views.py
+++ api/views.py
@@ -2,9 +2,11 @@
 import os
 import requests
 import time
-from django.shortcuts import render
+import json
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from FaceGen import *
 
 API_KEY = 'WRg5cwms3We9MiZV9CHaJZH53kc30VFI'
 API_SECRET = 'a1S4bXBNjNZWHFAJRZUclUuYuaIV-aps'
@@ -12,11 +14,13 @@ URL = 'https://api-cn.faceplusplus.com/facepp/v3/detect?api_key=%s&api_secret=%s
       'return_attributes=gender,age,eyestatus,emotion,ethnicity,beauty,skinstatus' \
       '&return_landmark=%d' \
       % (API_KEY, API_SECRET, 2)
-FILE_PATH = "F://webroot"
+FILE_PATH = "D:\webroot"
 
 
 @csrf_exempt
 def get_base_features(request):
+    ip = get_ip(request)
+
     if request.method == "POST":
         img = request.FILES.get("img", None)
         if not img:
@@ -30,4 +34,20 @@ def get_base_features(request):
     destination.close()
     files = {'image_file': open(img_path, 'rb')}
     r = requests.post(url=URL, files=files).json().get('faces')
-    return HttpResponse(r)
+
+    if ip not in request.session:
+        value = {"img_path": img_path}
+        request.session["ip"] = value
+    else:
+        request.session["ip"]["img_path"] = img_path
+
+    json_str = json.dumps(r)
+    return HttpResponse(json_str)
+
+
+def get_ip(request):
+    if 'HTTP_X_FORWARDED_FOR' in request.META:
+        ip = request.META['HTTP_X_FORWARDED_FOR']
+    else:
+        ip = request.META['REMOTE_ADDR']
+    return ip
