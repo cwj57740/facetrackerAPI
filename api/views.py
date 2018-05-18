ef18421e2f827e55ae7cd90d2627bc9a4b9258c3
cwj57740 <chenweiji57740@gmail.com>
2018-05-18 17:24:33 +0800

    fix views.py

--- api/views.py
+++ api/views.py
@@ -15,13 +15,19 @@ from FaceGen import face_features
 from FaceGen import PixMain
 from FaceGen import StarMain
 
+# face++ api_key与api_secret
 API_KEY = 'WRg5cwms3We9MiZV9CHaJZH53kc30VFI'
 API_SECRET = 'a1S4bXBNjNZWHFAJRZUclUuYuaIV-aps'
+
+# face++ detect api
 URL = 'https://api-cn.faceplusplus.com/facepp/v3/detect?api_key=%s&api_secret=%s&' \
       'return_attributes=gender,age,eyestatus,emotion,ethnicity,beauty,skinstatus' \
       '&return_landmark=%d' \
       % (API_KEY, API_SECRET, 2)
+
+#face++ search api
 SEARCH_URL = 'https://api-cn.faceplusplus.com/facepp/v3/search'
+# search api 提交参数
 params = {
     'api_key': API_KEY,
     'api_secret': API_SECRET,
@@ -29,12 +35,21 @@ params = {
     'return_result_count': '3'
 }
 
+# token与图片匹配数据文件
 DATA_FILE = "data.txt"
+
+# 项目图片路径
 FILE_PATH = "E:\webroot"
 
 
 @csrf_exempt
 def get_base_features(request):
+    """
+    获取基本特征
+    :param request:
+    :return:
+    """
+    # 错误处理
     if request.method == "POST":
         if "img" in request.POST:
             img = request.POST["img"]
@@ -42,6 +57,8 @@ def get_base_features(request):
             return JsonResponse({"msg": "no img upload"})
     else:
         return JsonResponse({"msg": "method is not allowed "})
+
+    # 文件路径生成
     file_name = img
     ext = ".png"
     img_path = os.path.join(FILE_PATH, file_name+ext)
@@ -53,6 +70,7 @@ def get_base_features(request):
     json_str = "null"
     t = 0
     while json_str == "null":
+        # face++ detect api 调用
         r = requests.post(url=URL, files=files).json().get('faces')
         json_str = json.dumps(r)
         t += 1
@@ -66,6 +84,12 @@ def get_base_features(request):
 
 @csrf_exempt
 def get_stick_pic(request):
+    """
+    获取生成简笔画路径
+    :param request:
+    :return:
+    """
+    # 错误处理
     if request.method == "POST":
         if "json_str" in request.POST:
             json_str = request.POST["json_str"]
@@ -75,9 +99,12 @@ def get_stick_pic(request):
         return JsonResponse({"msg": "method is not allowed "})
     if "img_path" not in request.session:
         return JsonResponse({"msg": "use get_base_features first"})
+
+    # session 读取路径
     img_path = request.session["img_path"]
     print("img_path:"+img_path)
-    img_path = request.session["img_path"]
+
+    # 简笔画生成
     image = cv2.imread(img_path)
     image = face_features.convert(image)
     json_object = json.loads(json_str)
@@ -114,14 +141,22 @@ def get_stick_pic(request):
 
 @csrf_exempt
 def get_average_face(request):
+    """
+    根据简笔画生成平均脸
+    :param request:
+    :return:
+    """
+    # 错误处理
     if "stick_pic_path" not in request.session:
         return JsonResponse({"msg": "use get_stick_pic first"})
     stick_pic_path = request.session["stick_pic_path"]
     print("stick_pic_path:"+stick_pic_path)
 
+    # 获取简笔画
     stick_image = PixMain.Image.open(stick_pic_path)
     stick_image = PixMain.preprocess_img(stick_image, crop_to=256, resize_to=256, P=True)
 
+    # pix处理
     gen_image = PixMain.get_generator(enc_model="FaceGen/model/pix_enc.npz", dec_model="FaceGen/model/pix_dec.npz")
     name = str(int(time.time()))
     gen_image(stick_image, FILE_PATH, name)
@@ -143,6 +178,12 @@ def get_average_face(request):
 
 @csrf_exempt
 def change_features(request):
+    """
+    通过StarGens进一步根据特征生成人脸
+    :param request:
+    :return:
+    """
+    # 错误处理
     if request.method == "POST":
         if "list" in request.POST:
             list_str = request.POST["list"]
@@ -154,10 +195,12 @@ def change_features(request):
         return JsonResponse({"msg": "use get_average_face first"})
     average_face_path = request.session["average_face_path"]
 
+    # 获取参数
     list = json.loads(list_str)
 
     print("list_str:"+str(list))
 
+    # StarGen处理
     gen_image = StarMain.get_generator(model_path="FaceGen/model/star_gen.npz")
     image = StarMain.Image.open(average_face_path)
     image = StarMain.preprocess_img(image)
@@ -180,12 +223,18 @@ def change_features(request):
 
 @csrf_exempt
 def get_similar_face(request):
-    # result_img = request.session["result_img"]
-    result_img = request.session["average_face_path"]
+    """
+    找到图片库中相似人脸
+    :param request:
+    :return:
+    """
+    # 最终结果人脸路径获取
+    result_img = request.session["result_img"]
     print("result_img:" + result_img)
 
     i=0
     while True:
+        # face++ search api 调用
         files = {'image_file': open(result_img, 'rb')}
         r = requests.post(SEARCH_URL, data=params, files=files).json()
 
@@ -199,6 +248,7 @@ def get_similar_face(request):
         if i > 10:
             return JsonResponse({"msg": "timeout"})
     pitures = []
+    # 获取图片路径
     for item in results:
         if "face_token" in item:
             face_token = item["face_token"]
@@ -209,6 +259,11 @@ def get_similar_face(request):
 
 
 def get_pic_from_token(token):
+    """
+    根据token值获取图片名
+    :param token:
+    :return:
+    """
     path = FILE_PATH + "\\" + DATA_FILE
     f = open(path)
     lines = f.readlines()
